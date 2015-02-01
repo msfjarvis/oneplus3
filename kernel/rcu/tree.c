bb8022e103bb24afc42d10fbef60efadbb666e39
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2015-01-31 21:12:02 -0800

    rcu: Remove event tracing from rcu_cpu_notify(), used by offline CPUs
    Offline CPUs cannot safely invoke trace events, but such CPUs do execute
    within rcu_cpu_notify().  Therefore, this commit removes the trace events
    from rcu_cpu_notify().  These trace events are for utilization, against
    which rcu_cpu_notify() execution time should be negligible.
    
    Reported-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -3657,7 +3657,6 @@ static int rcu_cpu_notify(struct notifier_block *self,
 	struct rcu_node *rnp = rdp->mynode;
 	struct rcu_state *rsp;
 
-	trace_rcu_utilization(TPS("Start CPU hotplug"));
 	switch (action) {
 	case CPU_UP_PREPARE:
 	case CPU_UP_PREPARE_FROZEN:
@@ -3690,7 +3689,6 @@ static int rcu_cpu_notify(struct notifier_block *self,
 	default:
 		break;
 	}
-	trace_rcu_utilization(TPS("End CPU hotplug"));
 	return NOTIFY_OK;
 }
 
