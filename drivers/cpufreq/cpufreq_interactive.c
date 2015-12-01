8f5350849c8b6b53b876978b81dc64b811d2542c
andip71 <andreasp@gmx.de>
2015-12-01 06:27:30 +0100

    cpufreq_interactive: remove boost functionality
    This led to scaling issues where CPU clusters got stuck with some frequencies.
    
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -1160,7 +1160,7 @@ static ssize_t show_boost(struct cpufreq_interactive_tunables *tunables,
 static ssize_t store_boost(struct cpufreq_interactive_tunables *tunables,
 			   const char *buf, size_t count)
 {
-	int ret;
+/*	int ret;
 	unsigned long val;
 
 	ret = kstrtoul(buf, 0, &val);
@@ -1177,7 +1177,7 @@ static ssize_t store_boost(struct cpufreq_interactive_tunables *tunables,
 		tunables->boostpulse_endtime = ktime_to_us(ktime_get());
 		trace_cpufreq_interactive_unboost("off");
 	}
-
+*/
 	return count;
 }
 
