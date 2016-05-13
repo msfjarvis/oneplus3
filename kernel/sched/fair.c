f96bcfd94e2a220d7f0ae22e5c4e9b7d26401775
Joonwoo Park <joonwoop@codeaurora.org>
2016-05-13 14:08:40 -0700

    Revert "sched: set HMP scheduler's default initial task load to 100%"
    This reverts commit b67d1f4eb20ddb580dd88d2622563ac757cdddfd ("sched: set HMP scheduler's
    default initial task load to 100%") since 100% of init task load
    makes too much of power inefficiency on some targets.
    
    CRs-fixed: 1006303
    Change-Id: I81b4ba8fdc2e2fe1b40f18904964098fa558989b
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>
    Signed-off-by: ahmedradaideh <ahmed.radaideh@gmail.com>
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2341,7 +2341,7 @@ static inline void decay_scaled_stat(struct sched_avg *sa, u64 periods);
 /* Initial task load. Newly created tasks are assigned this load. */
 unsigned int __read_mostly sched_init_task_load_pelt;
 unsigned int __read_mostly sched_init_task_load_windows;
-unsigned int __read_mostly sysctl_sched_init_task_load_pct = 100;
+unsigned int __read_mostly sysctl_sched_init_task_load_pct = 15;
 
 unsigned int max_task_load(void)
 {
