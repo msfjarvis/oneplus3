482de6979e2879b85f1f27b57b66d5fb0cb1f1e9
engstk <eng.stk@sapo.pt>
2016-08-27 05:59:29 +0100

    drivers: misc: implement usb fast charge mode
    echo 0 /sys/kernel/fast_charge/force_fast_charge (disable)
    echo 1 /sys/kernel/fast_charge/force_fast_charge (enable)
    
    Signed-off-by: engstk <eng.stk@sapo.pt>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -43,6 +43,11 @@
 #include <linux/type-c_notifier.h>
 #include <linux/wakelock.h>
 #include <linux/proc_fs.h>
+
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
+
 #if defined(CONFIG_FB)
 #include <linux/notifier.h>
 #include <linux/fb.h>
@@ -2069,7 +2074,11 @@ static int smbchg_set_usb_current_max(struct smbchg_chip *chip,
 			}
 			chip->usb_max_current_ma = 500;
 		}
+#ifdef CONFIG_FORCE_FAST_CHARGE
+		if ((force_fast_charge > 0 && current_ma == CURRENT_500_MA) || current_ma == CURRENT_900_MA) {
+#else
 		if (current_ma == CURRENT_900_MA) {
+#endif
 			rc = smbchg_sec_masked_write(chip,
 					chip->usb_chgpth_base + CHGPTH_CFG,
 					CFG_USB_2_3_SEL_BIT, CFG_USB_3);
