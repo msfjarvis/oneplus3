c39469814f1a660fb2ca900b51eaa0a4b038a2d6
Sultanxda <sultanxda@gmail.com>
2016-12-17 01:52:30 +0100

    ARM: dts: 15811: Target different GPU firmware image name
    The OnePlus 3T uses GPU firmware images that have the same names as the ones
    for the OnePlus 3, but the OnePlus 3's TrustZone firmware refuses to load
    GPU firmware from the 3T. This allows a single ROM to carry the needed
    firmware to support both OnePlus 3T and OnePlus 3 simultaneously.
    
    Change-Id: I0cf56dca89db573ccef39231f1ff06f51052d01b
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/gpu/msm/adreno_a5xx.c
+++ drivers/gpu/msm/adreno_a5xx.c
@@ -2156,6 +2156,7 @@ static int a5xx_microcode_load(struct adreno_device *adreno_dev)
 	void *ptr;
 	struct kgsl_device *device = KGSL_DEVICE(adreno_dev);
 	uint64_t gpuaddr;
+	const char *zap_name;
 
 	gpuaddr = adreno_dev->pm4.gpuaddr;
 	kgsl_regwrite(device, A5XX_CP_PM4_INSTR_BASE_LO,
@@ -2192,8 +2193,11 @@ static int a5xx_microcode_load(struct adreno_device *adreno_dev)
 	}
 
 	/* Load the zap shader firmware through PIL if its available */
-	if (adreno_dev->gpucore->zap_name && !zap_ucode_loaded) {
-		ptr = subsystem_get(adreno_dev->gpucore->zap_name);
+	if (!zap_ucode_loaded) {
+		if (of_property_read_string(device->pdev->dev.of_node, "cm,firmware-name", &zap_name))
+			zap_name = adreno_dev->gpucore->zap_name;
+		if (zap_name)
+			ptr = subsystem_get(zap_name);
 
 		/* Return error if the zap shader cannot be loaded */
 		if (IS_ERR_OR_NULL(ptr))
