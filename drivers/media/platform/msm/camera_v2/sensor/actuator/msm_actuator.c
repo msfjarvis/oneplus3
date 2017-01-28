dad1c13e048c41f28492f9e8b77429753f7e39b8
Francisco Franco <franciscofranco.1990@gmail.com>
2017-01-28 19:12:37 +0000

    camera: shut up actuator, I don't want to know when you're focusing
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -502,7 +502,9 @@ static void msm_actuator_write_focus(
 
 	damping_code_step = damping_params->damping_step;
 	wait_time = damping_params->damping_delay;
-    pr_info ("cur=%d sign=%d step=%d\n",curr_lens_pos, sign_direction, damping_code_step);
+
+    	pr_debug("cur=%d sign=%d step=%d\n", curr_lens_pos, sign_direction, damping_code_step);
+
 	/* Write code based on damping_code_step in a loop */
 	for (next_lens_pos =
 		curr_lens_pos + (sign_direction * damping_code_step);
