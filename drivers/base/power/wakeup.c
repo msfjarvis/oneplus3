31a3f6a2d1530a111bd5dbb877bc6e4bd86bd3ba
Francisco Franco <franciscofranco.1990@gmail.com>
2017-03-23 17:55:53 +0000

    drivers: wakeup: there's no much point in running the blockers during screen on
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -16,6 +16,7 @@
 #include <linux/debugfs.h>
 #include <linux/types.h>
 #include <linux/moduleparam.h>
+#include <linux/display_state.h>
 #include <trace/events/power.h>
 
 #include "power.h"
@@ -466,7 +467,7 @@ static bool wakeup_source_blocker(struct wakeup_source *ws)
 {
 	unsigned int wslen = 0;
 
-	if (ws && ws->active) {
+	if (!is_display_on() && ws && ws->active) {
 		wslen = strlen(ws->name);
 
 		if ((!enable_ipa_ws && !strncmp(ws->name, "IPA_WS", wslen)) ||
