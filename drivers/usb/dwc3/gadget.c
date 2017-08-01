cf3d6c35d07ed8dee895e8b78863d6acd8fbb5d9
Nathan Chancellor <natechancellor@gmail.com>
2017-08-01 12:15:24 -0700

    Revert some incorrect fixes from 3.18.40
    The first commit was a fix for the second; however, the "fix" breaks
    MTP and Android Auto. Go back to CAF's solution for the issue.
    
    This reverts the following commits:
        - usb: dwc3: fix for the isoc transfer EP_BUSY flag
        - Revert "Revert "usb: dwc3: gadget: always enable IOC on bulk/interrupt transfers""
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -927,6 +927,9 @@ update_trb:
 			trb->ctrl = DWC3_TRBCTL_ISOCHRONOUS_FIRST;
 		else
 			trb->ctrl = DWC3_TRBCTL_ISOCHRONOUS;
+
+		if (!req->request.no_interrupt && !chain)
+			trb->ctrl |= DWC3_TRB_CTRL_IOC;
 		break;
 
 	case USB_ENDPOINT_XFER_BULK:
@@ -942,9 +945,6 @@ update_trb:
 		 */
 		BUG();
 	}
-
-	if (!req->request.no_interrupt && !chain)
-		trb->ctrl |= DWC3_TRB_CTRL_IOC;
 
 	if (usb_endpoint_xfer_isoc(dep->endpoint.desc)) {
 		trb->ctrl |= DWC3_TRB_CTRL_ISP_IMI;
@@ -2641,10 +2641,9 @@ static int dwc3_cleanup_done_reqs(struct dwc3 *dwc, struct dwc3_ep *dep,
 		return 1;
 	}
 
-	if (usb_endpoint_xfer_isoc(dep->endpoint.desc))
-		if ((event->status & DEPEVT_STATUS_IOC) &&
-				(trb->ctrl & DWC3_TRB_CTRL_IOC))
-			return 0;
+	if ((event->status & DEPEVT_STATUS_IOC) &&
+			(trb->ctrl & DWC3_TRB_CTRL_IOC))
+		return 0;
 	return 1;
 }
 
