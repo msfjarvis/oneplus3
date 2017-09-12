34fcb4d3a8bb4da4a7ac4b9fad1caf16828b56f5
Harsh Shandilya <msfjarvis@gmail.com>
2017-09-12 23:41:24 +0530

    drivers:cpufreq:qcom-cpufreq: Initialise all ints
    * Nathan has had some interesting findings about a similar
      situation in his OP5 kernel causing all frequencies to be
      stuck at max with GCC 7. While I do not have this bug,
      I do feel the frequencies tend to be on the higher side.
      Let's see if his critical fix helps me too or ends up as
      snake oil.
    
    Change-Id: I831eeb175975904f78c9f5d908897c0405d432a6
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- drivers/cpufreq/qcom-cpufreq.c
+++ drivers/cpufreq/qcom-cpufreq.c
@@ -48,7 +48,7 @@ static int set_cpu_freq(struct cpufreq_policy *policy, unsigned int new_freq,
 {
 	int ret = 0;
 	struct cpufreq_freqs freqs;
-	unsigned long rate;
+	unsigned long rate = 0L;
 
 	freqs.old = policy->cur;
 	freqs.new = new_freq;
@@ -72,7 +72,7 @@ static int msm_cpufreq_target(struct cpufreq_policy *policy,
 				unsigned int relation)
 {
 	int ret = 0;
-	int index;
+	int index = 0;
 	struct cpufreq_frequency_table *table;
 
 	mutex_lock(&per_cpu(suspend_data, policy->cpu).suspend_mutex);
@@ -131,12 +131,12 @@ static unsigned int msm_cpufreq_get_freq(unsigned int cpu)
 
 static int msm_cpufreq_init(struct cpufreq_policy *policy)
 {
-	int cur_freq;
-	int index;
+	int cur_freq = 0;
+	int index = 0;
 	int ret = 0;
 	struct cpufreq_frequency_table *table =
 			per_cpu(freq_table, policy->cpu);
-	int cpu;
+	int cpu = 0;
 
 	/*
 	 * In some SoC, some cores are clocked by same source, and their
@@ -184,7 +184,7 @@ static int msm_cpufreq_cpu_callback(struct notifier_block *nfb,
 		unsigned long action, void *hcpu)
 {
 	unsigned int cpu = (unsigned long)hcpu;
-	int rc;
+	int rc = 0;
 
 	/* Fail hotplug until this driver can get CPU clocks */
 	if (!hotplug_ready)
@@ -243,7 +243,7 @@ static struct notifier_block __refdata msm_cpufreq_cpu_notifier = {
 
 static int msm_cpufreq_suspend(void)
 {
-	int cpu;
+	int cpu = 0;
 
 	for_each_possible_cpu(cpu) {
 		mutex_lock(&per_cpu(suspend_data, cpu).suspend_mutex);
@@ -256,7 +256,7 @@ static int msm_cpufreq_suspend(void)
 
 static int msm_cpufreq_resume(void)
 {
-	int cpu, ret;
+	int cpu = 0, ret = 0;
 	struct cpufreq_policy policy;
 
 	for_each_possible_cpu(cpu) {
@@ -326,7 +326,7 @@ static struct cpufreq_driver msm_cpufreq_driver = {
 static struct cpufreq_frequency_table *cpufreq_parse_dt(struct device *dev,
 						char *tbl_name, int cpu)
 {
-	int ret, nf, i;
+	int ret = 0, nf = 0, i = 0;
 	u32 *data;
 	struct cpufreq_frequency_table *ftbl;
 
@@ -394,7 +394,7 @@ static int __init msm_cpufreq_probe(struct platform_device *pdev)
 	char clk_name[] = "cpu??_clk";
 	char tbl_name[] = "qcom,cpufreq-table-??";
 	struct clk *c;
-	int cpu;
+	int cpu = 0;
 	struct cpufreq_frequency_table *ftbl;
 
 	l2_clk = devm_clk_get(dev, "l2_clk");
@@ -478,7 +478,7 @@ static struct platform_driver msm_cpufreq_plat_driver = {
 
 static int __init msm_cpufreq_register(void)
 {
-	int cpu, rc;
+	int cpu = 0, rc = 0;
 
 	for_each_possible_cpu(cpu) {
 		mutex_init(&(per_cpu(suspend_data, cpu).suspend_mutex));
