8e35a91871dcf337c1d3298371b653fa105cb486
Sultanxda <sultanxda@gmail.com>
2017-09-18 10:32:15 -0700

    input: synaptics: s3320: Don't run baseline inside IRQ handler
    tp_baseline_get() disables the touchscreen IRQ, so it should not be called
    from within the IRQ handler it disables. Call it from inside a worker instead.
    
    Change-Id: I8fc6f096e1387b259b7e0723522a33ebe3e15329
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -466,6 +466,7 @@ struct synaptics_ts_data {
 	uint32_t pre_finger_state;
 	uint32_t pre_btn_state;
 	struct delayed_work  base_work;
+	struct work_struct base_work_intr;
 	struct delayed_work speed_up_work;
 	struct input_dev *input_dev;
 	struct hrtimer timer;
@@ -1345,7 +1346,7 @@ Left2RightSwip:%d Right2LeftSwip:%d Up2DownSwip:%d Down2UpSwip:%d\n",
 #ifdef REPORT_2D_PRESSURE
 static unsigned char pres_value = 1;
 #endif
-void int_touch(void)
+uint8_t int_touch(void)
 {
 	int ret = -1,i = 0;
 	uint8_t buf[90];
@@ -1483,12 +1484,6 @@ void int_touch(void)
 	}
 	input_sync(ts->input_dev);
 
-	if ((finger_num == 0) && (get_tp_base == 0)){//all finger up do get base once
-		get_tp_base = 1;
-		TPD_ERR("start get base data:%d\n",get_tp_base);
-		tp_baseline_get(ts, false);
-	}
-
 #ifdef SUPPORT_GESTURE
 	if (ts->in_gesture_mode == 1 && ts->is_suspended == 1) {
 		gesture_judge(ts);
@@ -1496,6 +1491,7 @@ void int_touch(void)
 #endif
     INT_TOUCH_END:
 	mutex_unlock(&ts->mutexreport);
+	return finger_num;
 }
 
 #ifndef TPD_USE_EINT
@@ -1610,8 +1606,16 @@ static irqreturn_t synaptics_irq_thread_fn(int irq, void *dev_id)
 		goto END;
 	}
 */
-	if ( inte & 0x04 )
-		int_touch();
+	if (inte & 0x04) {
+		uint8_t finger_num = int_touch();
+
+		/* All fingers up; do get base once */
+		if (!get_tp_base && !finger_num) {
+			get_tp_base = 1;
+			TPD_ERR("start get base data: 1\n");
+			schedule_work(&ts->base_work_intr);
+		}
+	}
 
 	return IRQ_HANDLED;
 }
@@ -3066,6 +3070,13 @@ static void tp_baseline_get_work(struct work_struct *work)
 	tp_baseline_get(ts, true);//get the delta data
 }
 
+static void tp_baseline_get_in_intr(struct work_struct *work)
+{
+	struct synaptics_ts_data *ts = ts_g;
+
+	tp_baseline_get(ts, false);
+}
+
 static ssize_t touch_press_status_read(struct file *file, char __user *user_buf, size_t count, loff_t *ppos)
 {
 	int ret = 0;
@@ -4237,6 +4248,7 @@ static int synaptics_ts_probe(struct i2c_client *client, const struct i2c_device
 		goto exit_createworkqueue_failed;
 	}
 	INIT_DELAYED_WORK(&ts->base_work,tp_baseline_get_work);
+	INIT_WORK(&ts->base_work_intr, tp_baseline_get_in_intr);
 
 	ret = synaptics_init_panel(ts); /* will also switch back to page 0x04 */
 	if (ret < 0) {
