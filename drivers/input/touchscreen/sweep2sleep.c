1e207b1455d186bab1155c685bc4b12aa32f92d7
Harsh Shandilya <msfjarvis@gmail.com>
2017-09-22 01:47:42 +0530

    drivers:input:touchscreen: Disable sweep2sleep by default
    Change-Id: I4ec710e45e39aae8231723ac6521a9fdbd70acf7
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- drivers/input/touchscreen/sweep2sleep.c
+++ drivers/input/touchscreen/sweep2sleep.c
@@ -1,5 +1,5 @@
 #include <linux/module.h>
-#include <linux/kernel.h>    
+#include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/input.h>
 #include <linux/delay.h>
@@ -25,7 +25,7 @@ MODULE_LICENSE("GPL");
 extern void set_vibrate(int value);
 
 // 1=sweep right, 2=sweep left, 3=both
-static int s2s_switch = 2;
+static int s2s_switch = 0;
 
 static int touch_x = 0, touch_y = 0, firstx = 0;
 static bool touch_x_called = false, touch_y_called = false;
