24cb422b74fe7c5f160dac5c04f3342a6c287199
Harsh Shandilya <msfjarvis@gmail.com>
2017-10-08 23:54:49 +0530

    drivers:leds: Use fb notifier in boeffla touchkey control in place of lcd_notifier
    Change-Id: I655bd4594961e16b004fef324ef119c92c58187b
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- drivers/leds/boeffla_touchkey_control.c
+++ drivers/leds/boeffla_touchkey_control.c
@@ -1,7 +1,7 @@
 /*
- * 
+ *
  * Boeffla touchkey control OnePlus3/OnePlus2
- * 
+ *
  * Author: andip71 (aka Lord Boeffla)
  *
  * This software is licensed under the terms of the GNU General Public
@@ -18,9 +18,12 @@
 /*
  * Change log:
  *
+ * 1.3.2 (08.10.2017)
+ *  - Remove lcd notifier in favor of fb notifier
+ *
  * 1.3.1 (06.09.2017)
  *	- Corrections of some pr_debug functions
- * 
+ *
  * 1.3.0 (30.08.2017)
  *	- Adjust to stock handling, where by default display touch does not
  *    light up the touchkey lights anymore
@@ -62,7 +65,7 @@
 #include <linux/workqueue.h>
 #include <linux/notifier.h>
 #include <linux/kobject.h>
-#include <linux/lcd_notify.h>
+#include <linux/fb.h>
 #include <linux/boeffla_touchkey_control.h>
 
 
@@ -76,11 +79,9 @@ int btkc_timeout = TIMEOUT_DEFAULT;			// default is rom controlled timeout
 int isScreenTouched = 0;
 int cacheBrightness = BRIGHTNESS_DEFAULT;
 
-static struct notifier_block lcd_notif;
-
 static void led_work_func(struct work_struct *unused);
 static DECLARE_DELAYED_WORK(led_work, led_work_func);
-
+static struct notifier_block fb_notif;
 
 /*****************************************/
 // internal functions
@@ -95,41 +96,43 @@ static void led_work_func(struct work_struct *unused)
 	cancel_delayed_work(&led_work);
 }
 
-
-static int lcd_notifier_callback(struct notifier_block *this,
-								unsigned long event, void *data)
+static int fb_notifier_callback(struct notifier_block *self,
+			unsigned long event, void *data)
 {
-	switch (event)
-	{
-		case LCD_EVENT_OFF_START:
-			pr_debug("Boeffla touch key control: screen off detected, disable touchkey led\n");
-			
-			// switch off LED and cancel any scheduled work
-			qpnp_boeffla_set_button_backlight(BRIGHTNESS_OFF);
-			cancel_delayed_work(&led_work);
-			break;
+	struct fb_event *evdata = data;
+	int *blank;
 
-		case LCD_EVENT_ON_START:
-			pr_debug("Boeffla touch key control: screen on detected\n");
+	if (evdata && evdata->data && event == FB_EVENT_BLANK) {
+		blank = evdata->data;
+		switch (*blank) {
+			case FB_BLANK_UNBLANK:
+				/* display on */
+                pr_debug("Boeffla touch key control: screen on detected\n");
+                // only if in touchkey+display mode, or touchkey_only but with kernel controlled
+                // timeout, switch on LED and schedule work to switch it off again
+                if ((btkc_mode == MODE_TOUCHKEY_DISP) ||
+                    ((btkc_mode == MODE_TOUCHKEY_ONLY) && (btkc_timeout != 0)))
+                {
+                    qpnp_boeffla_set_button_backlight(cacheBrightness);
 
-			// only if in touchkey+display mode, or touchkey_only but with kernel controlled
-			// timeout, switch on LED and schedule work to switch it off again
-			if ((btkc_mode == MODE_TOUCHKEY_DISP) ||
-				((btkc_mode == MODE_TOUCHKEY_ONLY) && (btkc_timeout != 0)))
-			{
-				qpnp_boeffla_set_button_backlight(cacheBrightness);
-
-				cancel_delayed_work(&led_work);
-				schedule_delayed_work(&led_work, msecs_to_jiffies(btkc_timeout));
-			}
-
-		default:
-			break;
+                    cancel_delayed_work(&led_work);
+                    schedule_delayed_work(&led_work, msecs_to_jiffies(btkc_timeout));
+                }
+                break;
+            case FB_BLANK_POWERDOWN:
+			case FB_BLANK_HSYNC_SUSPEND:
+			case FB_BLANK_VSYNC_SUSPEND:
+			case FB_BLANK_NORMAL:
+				/* display off */
+				pr_debug("Boeffla touch key control: screen off detected, disable touchkey led\n");
+                // switch off LED and cancel any scheduled work
+                qpnp_boeffla_set_button_backlight(BRIGHTNESS_OFF);
+                cancel_delayed_work(&led_work);
+                break;
+		}
 	}
-
 	return 0;
 }
-
 
 /*****************************************/
 // exported functions
@@ -140,7 +143,7 @@ void btkc_touch_start()
 	pr_debug("Boeffla touch key control: display touch start detected\n");
 
 	isScreenTouched = 1;
-	
+
 	// only if in touchkey+display mode, switch LED on and cancel any scheduled work
 	if (btkc_mode == MODE_TOUCHKEY_DISP)
 	{
@@ -193,7 +196,7 @@ int btkc_led_set(int val)
 
 	// rom is only allowed to control LED when in touchkey_only mode
 	// and no kernel based timeout
-	if ((btkc_mode != MODE_TOUCHKEY_ONLY) || 
+	if ((btkc_mode != MODE_TOUCHKEY_ONLY) ||
 		((btkc_mode == MODE_TOUCHKEY_ONLY) && (btkc_timeout != 0)))
 		return -1;
 
@@ -320,10 +323,10 @@ static int btk_control_init(void)
 		return 0;
 	}
 
-	// register callback for screen on/off notifier
-	lcd_notif.notifier_call = lcd_notifier_callback;
-	if (lcd_register_client(&lcd_notif) != 0)
-		pr_err("%s: Failed to register lcd callback\n", __func__);
+	/* register callback for screen on/off notifier */
+	fb_notif.notifier_call = fb_notifier_callback;
+	if (fb_register_client(&fb_notif) != 0)
+		pr_err("%s: Failed to register fb callback\n", __func__);
 
 	// Print debug info
 	printk("Boeffla touch key control: driver version %s started\n", BTK_CONTROL_VERSION);
@@ -341,8 +344,8 @@ static void btk_control_exit(void)
 	cancel_delayed_work(&led_work);
 	flush_scheduled_work();
 
-	// unregister screen notifier
-	lcd_unregister_client(&lcd_notif);
+	/* unregister screen notifier */
+	fb_unregister_client(&fb_notif);
 
 	// Print debug info
 	printk("Boeffla touch key control: driver stopped\n");
