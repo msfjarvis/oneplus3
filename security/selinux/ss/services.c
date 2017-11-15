a8865a385ca09a01d84e5a6ef20252f5e56ed955
Harsh Shandilya <msfjarvis@gmail.com>
2017-11-15 20:14:54 +0530

    security: selinux: Silence logspam
    * Pretty much every Oreo ROM is shipping as permissive
      right now with little to zero sepolicy additions
      making undefined classes pretty common. This logging
      destroys dmesg completely for me. Silencio!
    
    Change-Id: Ie452e1ba07c6a474fa7682b1b4b5f0e9b64ca509
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -164,8 +164,7 @@ static int selinux_set_mapping(struct policydb *pol,
 			p_out->perms[k] = string_to_av_perm(pol, p_out->value,
 							    p_in->perms[k]);
 			if (!p_out->perms[k]) {
-				printk(KERN_INFO
-				       "SELinux:  Permission %s in class %s not defined in policy.\n",
+				pr_debug("SELinux:  Permission %s in class %s not defined in policy.\n",
 				       p_in->perms[k], p_in->name);
 				if (pol->reject_unknown)
 					goto err;
