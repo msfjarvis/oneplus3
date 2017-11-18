bc38ae0605ad20924fb427dfca395356b7fb2e4a
Harsh Shandilya <msfjarvis@gmail.com>
2017-11-18 19:58:38 +0530

    PerfectPrunes
    Change-Id: I8b4e0ee6334f59c3c76ad4cebc5579ce747b9681
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ KERNEL="Image.gz-dtb"
 
 # Caesium Kernel Details
 KERNEL_NAME="Caesium"
-INCREMENTAL_VERSION="AwesomeApricot"
+INCREMENTAL_VERSION="PerfectPrunes"
 export LOCALVERSION=-${INCREMENTAL_VERSION}
 GIT_HASH=$(git rev-parse --short HEAD)
 DEVICE="oneplus3"
