7fe901e55f97ddcaa1010c9b259ae10033b0a135
Harsh Shandilya <msfjarvis@gmail.com>
2017-11-22 20:21:54 +0530

    AmazingAvocado
    Change-Id: I77ab38f3ac28e429c80a32fcee7b3d8922cfd251
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ KERNEL="Image.gz-dtb"
 
 # Caesium Kernel Details
 KERNEL_NAME="Caesium"
-INCREMENTAL_VERSION="BeautifulBreadfruit"
+INCREMENTAL_VERSION="AmazingAvocado"
 export LOCALVERSION=-${INCREMENTAL_VERSION}
 GIT_HASH=$(git rev-parse --short HEAD)
 DEVICE="oneplus3"
