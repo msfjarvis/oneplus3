29b9b6392b00f771cc7d42d74cd62dba808751c4
Sultanxda <sultanxda@gmail.com>
2017-11-29 01:23:18 -0800

    kthread: Force all non-percpu kthreads onto the power cluster
    Force all kthreads that don't need to be on a specific CPU onto the power
    cluster in order to save power. This overrides anyone (user-land and
    kernel-land) who wants to change the CPU affinity of a non-percpu kthread.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1851,6 +1851,9 @@ struct task_struct {
 	unsigned int	sequential_io;
 	unsigned int	sequential_io_avg;
 #endif
+
+	/* CPU-bound kernel thread */
+	bool kthread_per_cpu;
 };
 
 /* Future-safe accessor for struct task_struct's cpus_allowed. */
