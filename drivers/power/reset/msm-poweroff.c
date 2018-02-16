msfjarvis/oneplus3

commit d7e44694bcb31fe5e670bceadef0b48b06307f15

oneplus3: remove 'reboot edl' interface for security.
BUG: 63100473

Change-Id: If6cb5ce50524d34b30f09a22eeb76d2cd3a0e834
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

drivers/power/reset/msm-poweroff.c
@@ -151,7 +151,7 @@ static bool get_dload_mode(void)
 {
 	return dload_mode_enabled;
 }
-
+#if 0
 static void enable_emergency_dload_mode(void)
 {
 	int ret;
@@ -176,7 +176,7 @@ static void enable_emergency_dload_mode(void)
 	if (ret)
 		pr_err("Failed to set secure EDLOAD mode: %d\n", ret);
 }
-
+#endif
 static int dload_set(const char *val, struct kernel_param *kp)
 {
 	int ret;
@@ -330,8 +330,10 @@ static void msm_restart_prepare(const char *cmd)
 			if (!ret)
 				__raw_writel(0x6f656d00 | (code & 0xff),
 					     restart_reason);
+#if 0
 		} else if (!strncmp(cmd, "edl", 3)) {
 			enable_emergency_dload_mode();
+#endif
 		} else {
 			pr_notice("%s : cmd is %s, set to reboot mode\n", __func__, cmd);
 			qpnp_pon_set_restart_reason(PON_RESTART_REASON_REBOOT);

commit 2d526896a07dce9c32a8364c7038dc853ee3f29a

power: reset: msm-poweroff: Compile out enable_emergency_dload_mode
This method is not used anywhere after commit d7e44694bcb31fe5
so compile it out to resolve a build-time warning.

Fixes: d7e44694bcb31fe5 ("oneplus3: remove 'reboot edl' interface for security.")
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

drivers/power/reset/msm-poweroff.c
@@ -203,10 +203,12 @@ static void set_dload_mode(int on)
 	return;
 }
 
+#if 0
 static void enable_emergency_dload_mode(void)
 {
 	pr_err("dload mode is not enabled on target\n");
 }
+#endif
 
 static bool get_dload_mode(void)
 {