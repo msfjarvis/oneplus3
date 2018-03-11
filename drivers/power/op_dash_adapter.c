msfjarvis/oneplus3

commit 4c09b4dcb871006d99ce9779cb78c85f00d20dee

treewide: Fix compilation warnings
Resolve all compilation warnings from compiling with a
GCC 7.1.1 toolchain.

Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

drivers/power/op_dash_adapter.c
@@ -84,13 +84,14 @@ static void dash_uart_tx_bit(
 	case BIT_5:
 	case BIT_6:
 	case BIT_7:
-		if (tx_data & (1 << tx_bit))
+		if (tx_data & (1 << tx_bit)) {
 			dash_uart_gpio_set_value(
 			chip, chip->uart_tx_gpio, 1);
-		else
+		} else {
 			dash_uart_gpio_set_value(
 			chip, chip->uart_tx_gpio, 0);
 			tx_bit++;
+		}
 		break;
 	case BIT_STOP:
 	case BIT_IDLE:

arch/arm64/boot/Makefile
@@ -38,12 +38,14 @@ $(obj)/Image.gz: $(obj)/Image FORCE
 $(obj)/Image.gz-dtb: $(obj)/Image.gz $(DTB_OBJS) FORCE
 	$(call if_changed,cat)
 
+ifeq ($(BUILD_ARM64_UNCOMPRESSED_KERNEL),y)
 $(obj)/Image-dtb-hdr: $(obj)/Image FORCE
 	echo -n 'UNCOMPRESSED_IMG' > $@ && \
 	$(call size_append, $(filter-out FORCE,$^)) >> $@
 
 $(obj)/Image-dtb: $(obj)/Image-dtb-hdr $(obj)/Image $(DTB_OBJS) FORCE
 	$(call if_changed,cat)
+endif
 
 install: $(obj)/Image
 	$(CONFIG_SHELL) $(srctree)/$(src)/install.sh $(KERNELRELEASE) \

drivers/crypto/msm/qce50.c
@@ -4620,7 +4620,7 @@ again:
 			pce_dev->intr_cadence = 0;
 			atomic_set(&pce_dev->bunch_cmd_seq, 0);
 			atomic_set(&pce_dev->last_intr_seq, 0);
-			pce_dev->cadence_flag = ~pce_dev->cadence_flag;
+			pce_dev->cadence_flag = !pce_dev->cadence_flag;
 		}
 	}
 

drivers/esoc/esoc-mdm-drv.c
@@ -48,7 +48,7 @@ static int esoc_msm_restart_handler(struct notifier_block *nb,
 	struct mdm_drv *mdm_drv = container_of(nb, struct mdm_drv,
 					esoc_restart);
 	struct esoc_clink *esoc_clink = mdm_drv->esoc_clink;
-	const struct esoc_clink_ops const *clink_ops = esoc_clink->clink_ops;
+	const struct esoc_clink_ops *clink_ops = esoc_clink->clink_ops;
 	if (action == SYS_RESTART) {
 		if (mdm_dbg_stall_notify(ESOC_PRIMARY_REBOOT))
 			return NOTIFY_OK;
@@ -110,7 +110,7 @@ static void mdm_crash_shutdown(const struct subsys_desc *mdm_subsys)
 					container_of(mdm_subsys,
 							struct esoc_clink,
 								subsys);
-	const struct esoc_clink_ops const *clink_ops = esoc_clink->clink_ops;
+	const struct esoc_clink_ops *clink_ops = esoc_clink->clink_ops;
 	if (mdm_dbg_stall_notify(ESOC_PRIMARY_CRASH))
 		return;
 	clink_ops->notify(ESOC_PRIMARY_CRASH, esoc_clink);
@@ -123,7 +123,7 @@ static int mdm_subsys_shutdown(const struct subsys_desc *crashed_subsys,
 	struct esoc_clink *esoc_clink =
 	 container_of(crashed_subsys, struct esoc_clink, subsys);
 	struct mdm_drv *mdm_drv = esoc_get_drv_data(esoc_clink);
-	const struct esoc_clink_ops const *clink_ops = esoc_clink->clink_ops;
+	const struct esoc_clink_ops *clink_ops = esoc_clink->clink_ops;
 
 	if (mdm_drv->mode == CRASH || mdm_drv->mode == PEER_CRASH) {
 		if (mdm_dbg_stall_cmd(ESOC_PREPARE_DEBUG))
@@ -168,7 +168,7 @@ static int mdm_subsys_powerup(const struct subsys_desc *crashed_subsys)
 				container_of(crashed_subsys, struct esoc_clink,
 								subsys);
 	struct mdm_drv *mdm_drv = esoc_get_drv_data(esoc_clink);
-	const struct esoc_clink_ops const *clink_ops = esoc_clink->clink_ops;
+	const struct esoc_clink_ops *clink_ops = esoc_clink->clink_ops;
 	int timeout = INT_MAX;
 
 	if (!esoc_clink->auto_boot && !esoc_req_eng_enabled(esoc_clink)) {
@@ -220,7 +220,7 @@ static int mdm_subsys_ramdumps(int want_dumps,
 	struct esoc_clink *esoc_clink =
 				container_of(crashed_subsys, struct esoc_clink,
 								subsys);
-	const struct esoc_clink_ops const *clink_ops = esoc_clink->clink_ops;
+	const struct esoc_clink_ops *clink_ops = esoc_clink->clink_ops;
 
 	if (want_dumps) {
 		ret = clink_ops->cmd_exe(ESOC_EXE_DEBUG, esoc_clink);

drivers/esoc/esoc.h
@@ -76,7 +76,7 @@ struct esoc_clink {
 	struct platform_device *pdev;
 	unsigned int id;
 	struct module *owner;
-	const struct esoc_clink_ops const *clink_ops;
+	const struct esoc_clink_ops *clink_ops;
 	struct esoc_eng *req_eng;
 	struct esoc_eng *cmd_eng;
 	spinlock_t notify_lock;

drivers/esoc/esoc_dev.c
@@ -179,7 +179,7 @@ static long esoc_dev_ioctl(struct file *file, unsigned int cmd,
 	struct esoc_uhandle *uhandle = file->private_data;
 	struct esoc_udev *esoc_udev = uhandle->esoc_udev;
 	struct esoc_clink *esoc_clink = uhandle->esoc_clink;
-	const struct esoc_clink_ops const *clink_ops = esoc_clink->clink_ops;
+	const struct esoc_clink_ops *clink_ops = esoc_clink->clink_ops;
 	void __user *uarg = (void __user *)arg;
 
 	switch (cmd) {

drivers/i2c/busses/i2c-msm-v2.c
@@ -2850,7 +2850,7 @@ static const struct i2c_algorithm i2c_msm_frmwrk_algrtm = {
 	.functionality	= i2c_msm_frmwrk_func,
 };
 
-static const char const *i2c_msm_adapter_name = "MSM-I2C-v2-adapter";
+static const char *i2c_msm_adapter_name = "MSM-I2C-v2-adapter";
 
 static int i2c_msm_frmwrk_reg(struct platform_device *pdev,
 						struct i2c_msm_ctrl *ctrl)

drivers/input/touchscreen/synaptics_driver_s1302.c
@@ -1094,10 +1094,10 @@ static ssize_t synaptics_s1302_radd_write(struct file *file, const char __user *
         {
             printk("reg=0x%x\n",reg[i]);
         }
-    }
-    else
+    } else {
         block = temp_block;
-	return count;
+    }
+    return count;
 }
 static int synaptics_s1302_radd_open(struct inode *inode, struct file *file)
 {

commit e47e3237c7c88473ce8566c7d011cc4b114c7b00

power: op_dash_adapter: Fix tx_bit increment in dash_uart_tx_bit
As pointed out by Nathan, tx_bit should be incremented unconditionally
in all BIT_{0..7} cases. Restore this behaviour.

Fixes: 4c09b4dcb871 ("treewide: Fix compilation warnings")
Reported-by: Nathan Chancellor <nathan@prjkt.io>
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

drivers/power/op_dash_adapter.c
@@ -84,14 +84,13 @@ static void dash_uart_tx_bit(
 	case BIT_5:
 	case BIT_6:
 	case BIT_7:
-		if (tx_data & (1 << tx_bit)) {
+		if (tx_data & (1 << tx_bit))
 			dash_uart_gpio_set_value(
 			chip, chip->uart_tx_gpio, 1);
-		} else {
+		else
 			dash_uart_gpio_set_value(
 			chip, chip->uart_tx_gpio, 0);
-			tx_bit++;
-		}
+		tx_bit++;
 		break;
 	case BIT_STOP:
 	case BIT_IDLE: