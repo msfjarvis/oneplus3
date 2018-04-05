01f0955e27366b555d21222cd106efc111773b3f
Harsh Shandilya <harsh@prjkt.io>
2018-04-05 06:21:35 +0200

    media: v4l2-compat: Revert non-upstream changes
    These conflict with upstream 3.18.103 changes. Will
    be re-added after the merge.
    
    Revert "media: v4l2-compat-ioctl32: memset stack union in compat ioctl"
    
    This reverts commit 74024a934acb2a05e5ede234836bd7903719d61e.
    
    Revert "[media] v4l2-ioctl.c: Copy userptr for multiplanar buffers"
    
    This reverts commit 0459c65a7471419665744795a716a039b3d8228c.
    
    Revert "[media] v4l: Copy Reserved and bytesused in multi-planar API"
    
    This reverts commit 5a17adb1064d2319b2d5f97a1e68c59330e290fa.
    
    Revert "[media] v4l: Copy Reserved field in multi-planar API"
    
    This reverts commit d9ffcddb127d1ff7f3efb0bef7bbf9468583b70e.
    
    Revert "[media] v4l2-compat-ioctl32: Update v4l2_event32 structure"
    
    This reverts commit d8e910a93f2dd24d21fd1561f06c320ec6e9fe09.
    
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- drivers/media/v4l2-core/v4l2-compat-ioctl32.c
+++ drivers/media/v4l2-core/v4l2-compat-ioctl32.c
@@ -336,10 +336,6 @@ static int get_v4l2_plane32(struct v4l2_plane __user *up, struct v4l2_plane32 __
 
 	if (copy_in_user(up, up32, 2 * sizeof(__u32)) ||
 		copy_in_user(&up->data_offset, &up32->data_offset,
-				sizeof(__u32)) ||
-		copy_in_user(up->reserved, up32->reserved,
-				sizeof(up->reserved)) ||
-		copy_in_user(&up->length, &up32->length,
 				sizeof(__u32)))
 		return -EFAULT;
 
@@ -365,8 +361,6 @@ static int put_v4l2_plane32(struct v4l2_plane __user *up, struct v4l2_plane32 __
 				enum v4l2_memory memory)
 {
 	if (copy_in_user(up32, up, 2 * sizeof(__u32)) ||
-		copy_in_user(up32->reserved, up->reserved,
-				sizeof(up32->reserved)) ||
 		copy_in_user(&up32->data_offset, &up->data_offset,
 				sizeof(__u32)))
 		return -EFAULT;
@@ -381,10 +375,6 @@ static int put_v4l2_plane32(struct v4l2_plane __user *up, struct v4l2_plane32 __
 	if (memory == V4L2_MEMORY_DMABUF)
 		if (copy_in_user(&up32->m.fd, &up->m.fd,
 					sizeof(int)))
-			return -EFAULT;
-	if (memory == V4L2_MEMORY_USERPTR)
-		if (copy_in_user(&up32->m.userptr, &up->m.userptr,
-					sizeof(compat_long_t)))
 			return -EFAULT;
 
 	return 0;
@@ -741,9 +731,6 @@ static int put_v4l2_ext_controls32(struct v4l2_ext_controls *kp, struct v4l2_ext
 struct v4l2_event32 {
 	__u32				type;
 	union {
-		struct v4l2_event_vsync		vsync;
-		struct v4l2_event_ctrl		ctrl;
-		struct v4l2_event_frame_sync	frame_sync;
 		__u8			data[64];
 	} u;
 	__u32				pending;
@@ -852,7 +839,6 @@ static long do_video_ioctl(struct file *file, unsigned int cmd, unsigned long ar
 	int compatible_arg = 1;
 	long err = 0;
 
-	memset(&karg, 0, sizeof(karg));
 	/* First, convert the command. */
 	switch (cmd) {
 	case VIDIOC_G_FMT32: cmd = VIDIOC_G_FMT; break;
