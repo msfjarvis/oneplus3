195299ca3f01b9b6fd035d0b1f29da49847baaf5
Harsh Shandilya <harsh@prjkt.io>
2018-04-05 06:50:32 +0200

    Revert "media: v4l2-compat: Revert non-upstream changes"
    Bring back CAF changes with context adjustments for -stable
    commits. Adjust some indentation to use tabs in place of
    mixed tabs and spaces.
    
    This reverts commit 01f0955e27366b555d21222cd106efc111773b3f.
    
    Reviewed-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- drivers/media/v4l2-core/v4l2-compat-ioctl32.c
+++ drivers/media/v4l2-core/v4l2-compat-ioctl32.c
@@ -382,9 +382,13 @@ static int get_v4l2_plane32(struct v4l2_plane __user *up,
 	compat_ulong_t p;
 
 	if (copy_in_user(up, up32, 2 * sizeof(__u32)) ||
-	    copy_in_user(&up->data_offset, &up32->data_offset,
-			 sizeof(up->data_offset)))
-		return -EFAULT;
+		copy_in_user(&up->data_offset, &up32->data_offset,
+				sizeof(up->data_offset)) ||
+		copy_in_user(up->reserved, up32->reserved,
+				sizeof(up->reserved)) ||
+		copy_in_user(&up->length, &up32->length,
+				sizeof(__u32)))
+			return -EFAULT;
 
 	switch (memory) {
 	case V4L2_MEMORY_MMAP:
@@ -414,9 +418,11 @@ static int put_v4l2_plane32(struct v4l2_plane __user *up,
 	unsigned long p;
 
 	if (copy_in_user(up32, up, 2 * sizeof(__u32)) ||
-	    copy_in_user(&up32->data_offset, &up->data_offset,
-			 sizeof(up->data_offset)))
-		return -EFAULT;
+		copy_in_user(&up32->data_offset, &up->data_offset,
+				sizeof(up->data_offset)) ||
+		copy_in_user(up32->reserved, up->reserved,
+				sizeof(up32->reserved)))
+			return -EFAULT;
 
 	switch (memory) {
 	case V4L2_MEMORY_MMAP:
@@ -893,6 +899,9 @@ static int put_v4l2_ext_controls32(struct file *file,
 struct v4l2_event32 {
 	__u32				type;
 	union {
+		struct v4l2_event_vsync		vsync;
+		struct v4l2_event_ctrl		ctrl;
+		struct v4l2_event_frame_sync	frame_sync;
 		__u8			data[64];
 	} u;
 	__u32				pending;
