f341a27acffe278d9b2c0ffa8e144c15bade1a7c
Harsh Shandilya <harsh@prjkt.io>
2018-04-29 08:08:00 +0200

    cpufreq_interactive: Allow disabling the screen_off_max tunable
    Set screen_off_max to 0 to prevent maxfreq being altered
    at screen off.
    
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -746,7 +746,7 @@ static int cpufreq_interactive_speedchange_task(void *data)
 			}
 
 			if (unlikely(!display_on)) {
-			    if (ppol->target_freq > tunables->screen_off_max)
+			    if (!(tunables->screen_off_max == 0) && ppol->target_freq > tunables->screen_off_max)
 				ppol->target_freq = tunables->screen_off_max;
 			}
 
@@ -1460,6 +1460,8 @@ static ssize_t store_screen_off_maxfreq(
 
 	if (val < 384000)
 		tunables->screen_off_max = DEFAULT_SCREEN_OFF_MAX;
+	else if (val == 0)
+		tunables->screen_off_max = 0;
 	else
 		tunables->screen_off_max = val;
 
