2f15362a7fe19eb06843501134a59b975b5274fd
Chao Yu <yuchao0@huawei.com>
2018-05-30 00:20:39 +0800

    f2fs: make __f2fs_write_data_pages() static
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -3090,9 +3090,6 @@ int f2fs_fiemap(struct inode *inode, struct fiemap_extent_info *fieinfo,
 bool should_update_inplace(struct inode *inode, struct f2fs_io_info *fio);
 bool should_update_outplace(struct inode *inode, struct f2fs_io_info *fio);
 void f2fs_set_page_dirty_nobuffers(struct page *page);
-int __f2fs_write_data_pages(struct address_space *mapping,
-						struct writeback_control *wbc,
-						enum iostat_type io_type);
 void f2fs_invalidate_page(struct page *page, unsigned int offset,
 			unsigned int length);
 int f2fs_release_page(struct page *page, gfp_t wait);
